be42838e252f508e342f4e23b0ffc9a94ccd6645
aashishsingh <aashishsingh75.as@gmail.com>
2021-06-13 00:20:12 +0530

    - Secret Manager Test Push

--- rdd/scholaship_recipient_filter_join.py
+++ rdd/scholaship_recipient_filter_join.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession, Row
 from distutils.util import strtobool
 import os.path
 import yaml
+import boto3
+import json
 import sys
 
 if __name__ == '__main__':
@@ -9,6 +11,13 @@ if __name__ == '__main__':
     os.environ["PYSPARK_SUBMIT_ARGS"] = (
         '--packages "org.apache.hadoop:hadoop-aws:2.7.4" pyspark-shell'
     )
+
+    #Secret Manager
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(
+        SecretID='AWS/MyFirstSecret'
+    )
+    secretDict = json.load(response['SecretString'])
 
     # Create the SparkSession
     spark = SparkSession \
@@ -20,17 +29,14 @@ if __name__ == '__main__':
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
-    secret = open(app_secrets_path)
-    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
     # Setup spark to use s3
     hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
-    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+    hadoop_conf.set("fs.s3a.access.key", secretDict['access_key'])
+    hadoop_conf.set("fs.s3a.secret.key", secretDict['secret_access_key'])
 
     demographics_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
     finances_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/finances.csv")
